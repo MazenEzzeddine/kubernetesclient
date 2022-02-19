81a8dfc80029d11b18204ee17e49076b57bec366
mazen <mazen.ezzeddine@gmail.com>
2022-02-19 11:38:47 +0100

    A kubernetes client. Dynamic pod creation.

---
+++ src/main/java/org/hps/Main.java
@@ -0,0 +1,61 @@
+package org.hps;
+
+
+
+
+import io.fabric8.kubernetes.api.model.*;
+
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        logger.info("Inside Main");
+
+        KubernetesClient client = new DefaultKubernetesClient();
+
+        // list pods in the default namespace
+        PodList pods = client.pods().inNamespace("default").list();
+        pods.getItems().stream().forEach(s -> System.out.println("Found pod: " + s.getMetadata().getName()));
+
+        // create a pod
+        System.out.println("Creating a pod");
+        Pod pod = client.pods().inNamespace("default").create(new PodBuilder()
+                .withNewMetadata()
+                .withName("my-programmatically-created-pod")
+                .endMetadata()
+                .withNewSpec()
+                .addNewContainer()
+                .withName("main")
+                .withImage("busybox")
+                .withCommand(Arrays.asList("sleep", "99999"))
+                .endContainer()
+                .endSpec()
+                .build());
+        System.out.println("Created pod: " + pod);
+
+        // edit the pod (add a label to it)
+        client.pods().inNamespace("default").withName("my-programmatically-created-pod").edit(p-> new PodBuilder(p)
+                .editMetadata()
+                .addToLabels("foo", "bar")
+                .and().build());
+
+        logger.info("Added label foo=bar to pod");
+
+        System.out.println("Waiting 1 minute before deleting pod...");
+        Thread.sleep(60000);
+
+        // delete the pod
+        client.pods().inNamespace("default").withName("my-programmatically-created-pod").delete();
+        System.out.println("Deleted the pod");
+    }
+
+}
+
